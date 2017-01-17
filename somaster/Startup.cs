0e66c8d870065ded92249ce1cfa9ec7bba437419
蔣 隆運 <miller0902@hotmail.com>
2017-01-17 17:01:16 +0800

    加入專案檔案。

---
+++ somaster/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(somaster.Startup))]
+namespace somaster
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
